ddee5144f8c47aeb3d42cb994f4c50fb0bb0a2cd
Athul Seban <athulseban94@gmail.com>
2026-02-06 10:08:15 +0000

    Merged main into dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   // Empty to allow DataService auto-detection for Codespaces
-  apiUrl: '' 
+  apiUrl: 'https://musical-space-guide-j49rw6qr6xq2jg97-8000.app.github.dev' 
 };
